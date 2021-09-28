89c1b75fe81fff4c114adac40b9c92a513e6d5bc
Will Barnes <will.t.barnes@gmail.com>
2021-09-28 15:25:47 -0400

    clean up EISMap class

--- eispac/core/eismap.py
+++ eispac/core/eismap.py
@@ -1,20 +1,22 @@
-__all__ = ['EISMap']
-
+"""
+`~sunpy.map.Map` subclass for the EUV Imaging Spectrometer (EIS) on Hinode
+"""
 import pathlib
+
+import astropy.units as u
+from astropy.visualization import ImageNormalize, AsinhStretch
 import numpy as np
 import sunpy.map
-from astropy.visualization import ImageNormalize, AsinhStretch
+from sunpy.time import parse_time
 
-"""
- A class to help sunpy maps properly plot EIS intensity fits files.
+__all__ = ['EISMap']
 
- v0.1 - 02-SEP-2021 : Original version, Will Barnes
-"""
 
 class EISMap(sunpy.map.GenericMap):
-    """EIS fit parameter map.
+    """
+    EIS fit parameter map.
 
-    The EUV Imaging Spectrometer is part of the Hinode mission and was sponsored
+    The EUV Imaging Spectrometer (EIS) is part of the Hinode mission and was sponsored
     by the Japan Aerospace Exploration Agency (JAXA), the United Kingdom Space
     Agency (UKSA), and National Aeronautics and Space Administration (NASA) with
     contributions from ESA and Norway. Hinode was launched on September 22, 2006
@@ -23,10 +25,14 @@ class EISMap(sunpy.map.GenericMap):
     171—212Å and 245—291Å with a spectral resolution of about 22mÅ and a plate
     scale of 100 per pixel.
 
+    This data structure is designed to hold the fit parameters derived from multi-gaussian
+    spectral fits to level 1, wavelength-resolved EIS rasters. These maps can contain the
+    intensity, doppler velocity, or line width.
+
     Notes
     -----
     Measurement errors are stored in a binary table. To load them correctly,
-    you must pass the .fits file directly to eispac.EISMap instead of using
+    you must pass the .fits file directly to `eispac.EISMap` instead of using
     sunpy.map.Map
 
     References
@@ -60,21 +66,25 @@ class EISMap(sunpy.map.GenericMap):
         self.meta['cunit1'] = self.meta.get('cunit1', 'arcsec')
         self.meta['cunit2'] = self.meta.get('cunit2', 'arcsec')
         self.meta['waveunit'] = self.meta.get('waveunit', 'angstrom')
-        self.meta['date-beg'] = self.meta['date_obs']
-        self.meta['date-end'] = self.meta['date_end']
-
-        # Want our coordinate frames to be constructed from DATE_AVG
-        # and not DATE_OBS as this is the start of the observing run
-        # del self.meta['date-obs'] # should we leave these for now, some routines expect them?
-        # del self.meta['date_obs']
-        # del self.meta['date_end']
+        self.meta['date-beg'] = self.meta.get('date_beg', self.meta.get('date_obs'))
+        self.meta['date-end'] = self.meta.get('date_end')
+        self.meta['date-obs'] = self.meta.get('date_obs')
+        self.meta['date-avg'] = self.meta.get('date_avg')
+        # NOTE: this block can be removed once sunpy>=3.1 is only supported as 
+        # the .date_average property will always be constructed in this way if
+        # date_start and date_end are present
+        if self.meta['date-avg'] is None:
+            if self.meta['date-beg'] is not None and self.meta['date-end'] is not None:
+                timesys = self.meta.get('timesys', 'UTC').lower()
+                start = parse_time(self.meta['date-beg'], scale=timesys)
+                end = parse_time(self.meta['date-end'], scale=timesys)
+                self.meta['date-avg'] = (start + (end - start)/2).isot
 
         # Setup plot settings
         self.plot_settings['aspect'] = self.meta['CDELT2'] / self.meta['CDELT1']
-        self.plot_settings['norm'] = ImageNormalize(stretch=AsinhStretch())
-        self.plot_settings['interpolation'] = 'kaiser'
         if self.meta['measrmnt'].lower().startswith('int'):
             self.plot_settings['cmap'] = 'Blues_r'
+            self.plot_settings['norm'] = ImageNormalize(stretch=AsinhStretch())
         elif self.meta['measrmnt'].lower().startswith('vel'):
             self.plot_settings['cmap'] = 'RdBu_r'
         elif self.meta['measrmnt'].lower().startswith('wid'):
@@ -82,8 +92,10 @@ class EISMap(sunpy.map.GenericMap):
 
     @classmethod
     def is_datasource_for(cls, data, header, **kwargs):
-        """Determines if header corresponds to an EIS image. Used to register
-        EISMap with the sunpy.map.Map factory."""
+        """
+        Determines if header corresponds to an EIS image. Used to register
+        EISMap with the sunpy.map.Map factory.
+        """
         return str(header.get('instrume', '')).startswith('EIS')
 
     @property
@@ -92,4 +104,31 @@ class EISMap(sunpy.map.GenericMap):
 
     @property
     def measurement(self):
-        return self.meta.get('line_id', '')
+        line_id = self.meta.get('line_id', '')
+        quantity = self.meta.get('measrmnt', '')
+        return  f'{line_id} {quantity}'
+
+    @property
+    def wavelength(self):
+        line_id = self.meta.get('line_id')
+        if line_id is not None:
+            wave = float(line_id.split()[-1])
+            return u.Quantity(wave, self.meta['waveunit'])
+
+    @property
+    def date(self):
+        # Want to make sure we are constructing our coordinate frames
+        # from DATE_AVG and not the DATE-OBS which is the beginning of
+        # the observation
+        # In sunpy 3.1, this may not be needed as we could just remove
+        # date-obs and then .date will default to .date_average
+        t = self.meta.get('date-avg')
+        timesys = self.meta.get('timesys', 'UTC')
+        if t is None:
+            return super().date
+        else:
+            return parse_time(t, scale=timesys.lower())
+
+    @property
+    def processing_level(self):
+        return self.meta.get('lvl_num', 3)
